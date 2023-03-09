233dc34c7c5711640e78da8a86ea31b67e003afe
tallahmad047 <tallahmad047@gmail.com>
2023-03-09 18:35:24 +0000

    initial

---
+++ src/test/java/org/sid/demokubernetes/DemokubernetesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.demokubernetes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemokubernetesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
